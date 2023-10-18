7e91b3b888529cbc93623729fbbc3eadff526fc4
blacksmokezip <hanzomainer1488@gmail.com>
2023-10-18 22:36:31 +1000

    Add Hello World view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from task_manager import views
 
 urlpatterns = [
-    path('', include('render.urls')),
+    path('', views.index),
+    path('render/', include('render.urls')),
     path('admin/', admin.site.urls),
 ]
