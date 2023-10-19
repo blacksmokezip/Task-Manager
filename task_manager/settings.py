fea5070cbba780682d78aae67d11b17f89e5b046
blacksmokezip <hanzomainer1488@gmail.com>
2023-10-19 16:28:59 +1000

    Update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/task_manager',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
