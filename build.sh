7f37a41a4ccfc507fb45ddfa9ac7e6a6cb8b9404
blacksmokezip <hanzomainer1488@gmail.com>
2023-11-02 13:53:49 +1000

    Update

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 poetry install
 
-poetry run python3 manage.py collectstatic --no-input
-poetry run python3 manage.py makemigrations
-poetry run python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  poetry run python3 manage.py createsuperuser --no-input
+  python3 manage.py createsuperuser --no-input
 fi
