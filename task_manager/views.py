268c4ecec55cf926087a87de4a6ef36e55efbbca
blacksmokezip <hanzomainer1488@gmail.com>
2023-11-03 21:43:51 +1000

    Fix linter errors

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth import logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
