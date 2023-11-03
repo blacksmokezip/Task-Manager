9213739b21d9f949accd567391844d253202b969
blacksmokezip <hanzomainer1488@gmail.com>
2023-11-03 18:08:12 +1000

    Fix bugs

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,23 +1,15 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
-from task_manager.tasks.models import Task
+from .models import Task
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('name', 'description', 'status', 'executor', 'labels')
-        widgets = {
-            'name': forms.TextInput(attrs={'placeholder': _("Name")}),
-            'description': forms.Textarea(attrs={
-                'placeholder': _("Description")
-            }),
-        }
-        labels = {
-            'name': _("Name"),
-            'description': _("Description"),
-            'status': _("Status"),
-            'executor': _("Executor"),
-            'labels': _("Labels"),
-        }
+        fields = (
+            'name',
+            'description',
+            'status',
+            'executor',
+            'labels'
+        )
